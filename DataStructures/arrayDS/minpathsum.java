ce09f5a800b1875487db97a28e1ff4db5b493bcd
bugaliaa <sidbugalia@gmail.com>
2020-11-02 11:36:21 +0530

    Insert Interval and Minimum Path Sum

--- DataStructures/arrayDS/minpathsum.java
+++ DataStructures/arrayDS/minpathsum.java
@@ -5,27 +5,38 @@ public class minpathsum {
         
     }
 
-    public int solveFunction(int[][] grid){
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
-        if(rows == 0) return 0;
+    public int minPathSum(int[][] grid) {        
+        if(grid.length == 0) return 0;
 
-        int dp[][] = new int[rows][cols];
+        for(int i = 1; i < grid[0].length; i++) grid[0][i] = grid[0][i] + grid[0][i-1];
 
-        dp[0][0] = grid[0][0];
+        for(int i = 1; i < grid.length; i++) grid[i][0] = grid[i][0] + grid[i-1][0];
 
-        //Sum of First row
-        for(int i = 1; i < cols; i++) dp[0][i] = grid[0][i] + dp[0][i-1];
-
-        //Sum of First Col
-        for(int i = 1; i < rows; i++) dp[i][0] = grid[i][0] + dp[i-1][0];
-
-        //
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < cols; j++) dp[i][j] = grid[i][j] + Math.min(grid[i-1][j], grid[i][j-1]);
+        for(int i = 1; i < grid.length; i++){
+            for(int j = 1; j < grid[0].length; j++) grid[i][j] = grid[i][j] + Math.min(grid[i-1][j], grid[i][j-1]);
         }
 
-        return grid[rows-1][cols-1];
+        return grid[grid.length-1][grid[0].length-1];
     }
+    int[][] table = null;
+    public int minPathSum2(int[][] grid) {        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        table = new int[rows][cols];
+
+        return helper(grid, grid.length-1, grid[0].length-1);
+    }
+
+    public int helper(int[][] grid, int row, int col){
+        if(row < 0 || col < 0) return Integer.MAX_VALUE;
+
+        if(row == 0 && col == 0) return grid[row][col];
+        
+        if(table[row][col] != 0) return table[row][col];
+
+        table[row][col] = grid[row][col] + Math.min(helper(grid, row-1, col), helper(grid, row, col-1));
+
+        return table[row][col];
+    }
+
 }
