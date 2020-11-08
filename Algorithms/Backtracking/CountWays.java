766dd9270e332c7b8327190f415db9c8e76322c9
Siddhant Bugalia <sidbugalia@gmail.com>
2020-11-08 19:02:23 +0530

    Count ways to calculate number of ways to reach target by elements in array

--- Algorithms/Backtracking/CountWays.java
+++ Algorithms/Backtracking/CountWays.java
@@ -1,5 +1,17 @@
 package Algorithms.Backtracking;
 
 public class CountWays {
-    
+    public int countWays(int[] nums, int target){
+        return helper(nums, nums.length-1, target);
+    }
+    public int helper(int[] nums, int n, int target){
+        if(target == 0) return 1;
+        if(n < 0) return 0;
+
+        int exclude = helper(nums, n-1, target);
+
+        int include = helper(nums, n-1, target-nums[n]) + helper(nums, n-1, target+nums[n]);
+
+        return exclude + include;
+    }
 }
